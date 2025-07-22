3907a185ee38c72dfd9d002f08f60950aeaa8780
Anthony Vargas <thonyalope@gmail.com>
2025-07-22 00:08:41 -0600

    TS-01: Implement PUT endpoint to update user

--- controllers/users.js
+++ controllers/users.js
@@ -24,7 +24,22 @@ const createUser = async (req, res, next) => {
     }
 }
 
+const updateUser = async (req, res, next) => {
+    const { pk_user } = req.params;
+    const { name, status } = req.body;
+
+    try {
+        let user = await users.updateUser(parseInt(pk_user), name, status);
+        res.status(200).send(user);
+        next();
+    } catch (e) {
+        console.log(e.message);
+        res.sendStatus(500) && next(e);
+    }
+};
+
 module.exports = {
     getUser,
-    createUser
+    createUser,
+    updateUser
 }
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const routes = require('./routes')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const routes = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => res.send('App is working'))
+app.get('/', (req, res) => res.send('App is working'));
 
-app.use('/api', routes)
+app.use('/api', routes);
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('Server listening on port 3000'));
 
-module.exports = {
-    app
-}
+module.exports = { app };
