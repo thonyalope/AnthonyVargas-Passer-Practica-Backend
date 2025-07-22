e09046b74dd7df1236fe9c410997fba448d3940e
Anthony Vargas <thonyalope@gmail.com>
2025-07-22 01:38:32 -0600

    TS-07: Implement PUT endpoint to update transaction

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/users/:pk_user', users.getUser)
     .put('/users/:pk_user', users.updateUser)
     .delete('/users/:pk_user', users.deleteUser)
     .post('/transactions', transactions.createTransaction)
-    .get('/transactions/:pk_transaction', transactions.getTransaction);
+    .get('/transactions/:pk_transaction', transactions.getTransaction)
+    .put('/transactions/:pk_transaction', transactions.updateTransaction);
 
 
 module.exports = router
